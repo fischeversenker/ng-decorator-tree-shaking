45726fe443bab24fa3f921cbf696e1fd2c94ce40
Felix Hamann <felix.hamann@satellytes.com>
2022-10-26 16:23:09 +0200

    chore: import from dist folder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-import { MyLibraryModule } from 'projects/my-library/src/public-api';
+import { MyLibraryModule } from 'my-library';
 
 import { AppComponent } from './app.component';
 import { UnusedComponentComponent } from './unused-component.component';
